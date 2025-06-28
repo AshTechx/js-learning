f32198a5a0de5922595adfb6a5afd528f543cf35
Aashiya khan <aashiyakhan403@gmail.com>
2025-06-28 17:06:19 +0530

    adding 28th learning codes of js

--- js pracset1.js
+++ js pracset1.js
@@ -24,3 +24,94 @@ console.log(dictionary["respect"]);
 dictionary["aashiya"] ="ash";
 console.log(dictionary); 
 console.log(dictionary["demands"]);
+
+
+// creating array 
+let princess = ["aurora" , "rapunzel" , "snowhite" , "cindrella"];
+console.log(princess);
+princess[1] = "sleeping beauty";
+console.log(princess);
+
+//calculating average of an array 
+
+let arr = [85 , 97 , 44, 37 ,76, 60];
+let sum = 0;
+for(val of arr){
+    sum+=val;
+
+
+}
+console.log(sum/6);
+
+
+// offer on each array value of 10 percent 
+let prices = [250 , 645 ,300 , 900, 50];
+
+for (let i=0;i<prices.length;i++){
+    prices[i] = prices[i]-prices[i]*10/100;
+    console.log(prices[i]);
+}
+
+// array methos example 
+let companies = ["bloomberg" , "microsoft", "uber", "google","ibm" , "netflix"];
+let update = companies.shift();
+console.log(companies);
+companies.splice(1, 1, "ola");
+console.log(companies);
+
+companies.push("amazon");
+console.log(companies);
+
+// function definition 
+function addition(p,q){
+    console.log(p+q);
+}
+addition(1,3) // function call 
+
+// return type of function 
+function retfunc(x,y){
+    j =x+y;
+    return j ;
+
+}
+let content = retfunc(45,67);
+console.log(content);
+
+
+// arrow function of multiplication 
+
+const arrowMul = (g,h) => {
+    return g*h;
+}
+let store = arrowMul(5,6);
+console.log(store);
+
+// question practise 
+// function in_put(line){
+//    for (i=0;i<=line.length;i++){
+//     if (line[i]==="a" ||
+//         line[i]=== "e" ||
+//         line[i]==="i" || 
+//         line[i]==="o"||
+//         line[i]==="u"){
+//         console.log("yes"+ "=" + line[i]);
+//     }
+   
+//    }
+// }
+// in_put("hello guys")
+
+
+const in_put =(line) => {
+for (i=0;i<=line.length;i++){
+    if (line[i]==="a" ||
+        line[i]=== "e" ||
+        line[i]==="i" || 
+        line[i]==="o"||
+        line[i]==="u"){
+        console.log("yes"+ "=" + line[i]);
+        
+    }
+}
+}
+ in_put("hello guys");
---
+++ practiseset_3.js
@@ -0,0 +1,17 @@
+const students = {
+    harry : 98,
+    rohan : 70,
+    aakash: 73
+
+}
+for( let a in students){
+    console.log( "the value of " + a + students[a]);
+}
+
+// try again until user enters correct number 
+let input_1 = 30;
+let user_in = prompt("input the number lets try your luck");
+for( let i=0;i==user_in;i++){
+    console.log("try again");
+}
+
